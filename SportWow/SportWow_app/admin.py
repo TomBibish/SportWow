fa30bedb8aac2d7ebe039f97da813ad7c12d015a
TomBibish <tombibish8@gmail.com>
2022-03-01 19:26:06 +0200

    SportWowProject

--- SportWow/SportWow_app/admin.py
+++ SportWow/SportWow_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(League)
+admin.site.register(Coach)
+admin.site.register(Country)
+admin.site.register(Team)
+admin.site.register(TeamCoach)
+admin.site.register(TeamPlayer)
+admin.site.register(City)
+admin.site.register(Stadium)
